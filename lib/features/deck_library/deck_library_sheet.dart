2b799f2c03a855c805d4f4c1b0f5dcd3986d00ae
kefeh <kefehcollins@gmail.com>
2025-12-15 18:17:35 +0100

    Populate the list with static, placeholder deck data to visualize the layout

--- lib/features/deck_library/deck_library_sheet.dart
+++ lib/features/deck_library/deck_library_sheet.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:social_shuffle/features/deck_library/widgets/deck_list_item.dart';
 
 class DeckLibrarySheet extends StatelessWidget {
   const DeckLibrarySheet({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> _decks = [
+      {'title': '90s Hip Hop (System)', 'isSystem': true},
+      {'title': 'Movie Quotes (System)', 'isSystem': true},
+      {'title': 'My Custom Deck', 'isSystem': false},
+    ];
+
     return Container(
       padding: const EdgeInsets.all(16.0),
       child: Column(
@@ -15,7 +22,21 @@ class DeckLibrarySheet extends StatelessWidget {
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 16),
-          const Text('Deck list will be shown here.'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _decks.length,
+              itemBuilder: (context, index) {
+                final deck = _decks[index];
+                return DeckListItem(
+                  title: deck['title'],
+                  isSystemDeck: deck['isSystem'],
+                  onTap: () {
+                    // Handle deck selection
+                  },
+                );
+              },
+            ),
+          ),
           const SizedBox(height: 16),
           ElevatedButton.icon(
             onPressed: () {
