f35c08a1e99a5446f00eb4a4d4933ee731d663ec
kefeh <kefehcollins@gmail.com>
2025-12-15 23:01:27 +0100

    adds deck models and decks assets for seed

---
+++ lib/core/models/deck.dart
@@ -0,0 +1,47 @@
+import 'package:social_shuffle/core/models/card.dart';
+import 'package:uuid/uuid.dart';
+
+class Deck {
+  final String id;
+  final String title;
+  final String gameEngineId;
+  final bool isSystem;
+  final DateTime createdAt;
+  final List<Card> cards;
+
+  Deck({
+    String? id,
+    required this.title,
+    required this.gameEngineId,
+    this.isSystem = false,
+    DateTime? createdAt,
+    required this.cards,
+  })  : id = id ?? const Uuid().v4(),
+        createdAt = createdAt ?? DateTime.now();
+
+  factory Deck.fromJson(Map<String, dynamic> json) {
+    return Deck(
+      id: json['id'] as String?,
+      title: json['title'] as String,
+      gameEngineId: json['game_engine_id'] as String,
+      isSystem: json['is_system'] as bool? ?? false,
+      createdAt: json['created_at'] != null
+          ? DateTime.parse(json['created_at'] as String)
+          : null,
+      cards: (json['cards'] as List<dynamic>)
+          .map((e) => Card.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'game_engine_id': gameEngineId,
+      'is_system': isSystem,
+      'created_at': createdAt.toIso8601String(),
+      'cards': cards.map((e) => e.toJson()).toList(),
+    };
+  }
+}
